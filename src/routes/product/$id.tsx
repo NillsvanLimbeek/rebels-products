331092fd743bd5ee827bfe1172b412bb8f98d7b9
NillsvanLimbeek <nills.van.limbeek@live.nl>
2025-01-30 17:06:05 +0100

    feature: fetching data on product detail page

--- src/routes/product/$id.tsx
+++ src/routes/product/$id.tsx
@@ -1,10 +1,17 @@
+import { api } from "@/lib/services/api";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/product/$id")({
   component: RouteComponent,
+  loader: (e) => api.get("/products/{id}", { params: { id: e.params.id } }),
 });
 
 function RouteComponent() {
-  const { id } = Route.useParams();
-  return <div>Hello "/product/$id"! {id}</div>;
+  const product = Route.useLoaderData();
+
+  return (
+    <div>
+      <h1>{product.name}</h1>
+    </div>
+  );
 }
